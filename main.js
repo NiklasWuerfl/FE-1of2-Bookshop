2074ab12c3ec04160a31b6940da92d3f97b7c072
NiklasWuerfl <wi21162@lehre.dhbw-stuttgart.de>
2023-03-07 20:23:08 +0100

    First Filter and Sorting
    Filter for categories
    Sorting for Price and Title
    -- more need to be implemented! --

--- main.js
+++ main.js
@@ -6,28 +6,93 @@ async function getJSON(url) {
   return data
 }
 
-let books
+let books,
+  chosenCategoryFilter = 'all',
+  chosenSortOption,
+  categories = [];
 
 async function start() {
   books = await getJSON('/books.json')
+  getCategories();
+  addFilters();
+  addSortingOptions();
+  sortByTitle(books);
   displayBooks()
 }
 
-async function displayBooks() {
-  let html = ''
-  for (let book of books) {
-    html += `
-      <div class="book">
-        <h3>${book.title}</h3>
-        `
-    for (let key in book) {
-      let value = book[key]
-      html += `
-      <p><span>${key}:</span> ${value}</p>`
+function sortByTitle(books) {
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+    aTitle > bTitle ? 1 : -1);
+}
+
+function sortByPrice(books) {
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+    aPrice > bPrice ? 1 : -1);
+}
+
+function addSortingOptions() {
+  document.querySelector('.sortingOptions').innerHTML = /*html*/`
+    <label><span>Sort by:</span>
+      <select class="sortOption">
+        <option>Title</option>
+        <option>Price</option>
+      </select>
+    </label>
+  `;
+  document.querySelector('.sortOption').addEventListener('change', event => {
+    chosenSortOption = event.target.value;
+    displayBooks();
+  });
+}
+
+function getCategories() {
+  let withDuplicates = books.map(book => book.category);
+  categories = [...new Set(withDuplicates)];
+  categories.sort();
+}
+
+function addFilters() {
+  document.querySelector('.filters').innerHTML = /*html*/`
+    <label><span>Filter by categories:</span>
+      <select class="categoryFilter">
+        <option>all</option>
+        ${categories.map(category => `<option>${category}</option>`).join('')}
+      </select>
+    </label>
+  `;
+  document.querySelector('.categoryFilter').addEventListener(
+    'change',
+    event => {
+      // get the selected category
+      chosenCategoryFilter = event.target.value;
+      displayBooks();
     }
-    html += '</div>'
-  }
-  document.querySelector('main').innerHTML = html
+  );
+}
+
+function displayBooks() {
+  // filter according to category and call displayBooks
+  let filteredBooks = books.filter(
+    ({ category }) => chosenCategoryFilter === 'all'
+      || chosenCategoryFilter === category
+  );
+  // !! add filter for Author and Price Ranges !!
+  if (chosenSortOption === 'Title') { sortByLastName(filteredBooks); }
+  if (chosenSortOption === 'Price') { sortByPrice(filteredBooks); }
+  // !! add sort options for Author and DESCENDING for all of them !!
+  let htmlArray = filteredBooks.map(({
+    id, title, author, description, category, price
+  }) => /*html*/`
+    <div class="book">
+      <h3>${title}</h3>
+      <p><span>id</span>${id}</p>
+      <p><span>author</span>${author}</p>
+      <p><span>description</span>${description}</p>
+      <p><span>category</span>${category}</p>
+      <p><span>price</span>${price}</p>
+    </div>
+  `);
+  document.querySelector('.bookList').innerHTML = htmlArray.join('');
 }
 
 start()
