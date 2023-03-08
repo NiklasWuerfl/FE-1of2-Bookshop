c1ffa713558f1046c7002aaf99bb9ed8716fd6b0
NiklasWuerfl <wi21162@lehre.dhbw-stuttgart.de>
2023-03-08 12:13:31 +0100

    Full filter and sorting functionality
    All requirements for filtering and sorting have been implemented
    For the sorting, I chose to add an option to sort by ID
    
    Modifying JSON data, to have better example data.
    
    At the moment, all the code is in the main.js file. I will try to clean up in one of the next commits.

--- main.js
+++ main.js
@@ -8,16 +8,34 @@ async function getJSON(url) {
 
 let books,
   chosenCategoryFilter = 'all',
-  chosenSortOption,
-  categories = [];
+  chosenAuthorFilter = 'all',
+  chosenPriceFilter = 'all',
+  chosenSortOption = 'ID',
+  categories = [],
+  authors = [],
+  priceIntervals = ['0-40', '40-80', '80-120', '120-160'],
+  filteredBooks,
+  chosenSortOrder = 'asc';
+  
 
 async function start() {
   books = await getJSON('/books.json')
   getCategories();
+  getAuthors();
   addFilters();
   addSortingOptions();
   sortByTitle(books);
   displayBooks()
+}
+
+function sortByAuthor(books) {
+  books.sort(({ author: aAuthor }, { author: bAuthor }) =>
+    aAuthor > bAuthor ? 1 : -1);
+}
+
+function sortByAuthorDes(books) {
+  books.sort(({ author: aAuthor }, { author: bAuthor }) =>
+    aAuthor > bAuthor ? -1 : 1);
 }
 
 function sortByTitle(books) {
@@ -25,30 +43,70 @@ function sortByTitle(books) {
     aTitle > bTitle ? 1 : -1);
 }
 
+function sortByTitleDes(books) {
+  books.sort(({ title: aTitle }, { title: bTitle }) =>
+    aTitle > bTitle ? -1 : 1);
+}
+
 function sortByPrice(books) {
   books.sort(({ price: aPrice }, { price: bPrice }) =>
     aPrice > bPrice ? 1 : -1);
+}
+
+function sortByPriceDes(books) {
+  books.sort(({ price: aPrice }, { price: bPrice }) =>
+  aPrice > bPrice ? -1 : 1);
+}
+
+function sortById(books) {
+  books.sort(({ id: aId }, { id: bId }) =>
+    aId > bId ? 1 : -1);
+}
+
+function sortByIdDes(books) {
+  books.sort(({ id: aId }, { id: bId }) =>
+    aId > bId ? -1 : 1);
 }
 
 function addSortingOptions() {
   document.querySelector('.sortingOptions').innerHTML = /*html*/`
     <label><span>Sort by:</span>
       <select class="sortOption">
+        <option>ID</option>
+        <option>Author</option>
         <option>Title</option>
         <option>Price</option>
       </select>
+      <button class = "sortOrderAsc">Asc</button>
+      <button class = "sortOrderDes">Desc</button>
     </label>
   `;
   document.querySelector('.sortOption').addEventListener('change', event => {
     chosenSortOption = event.target.value;
+    chosenSortOrder = "asc";
     displayBooks();
-  });
+  })
+  document.querySelector('.sortOrderAsc').addEventListener('click', () => {
+    chosenSortOrder = "asc";
+    displayBooks();
+  })
+  document.querySelector('.sortOrderDes').addEventListener('click', () => {
+    chosenSortOrder = "des";
+    displayBooks();
+  })
+  
 }
 
 function getCategories() {
   let withDuplicates = books.map(book => book.category);
   categories = [...new Set(withDuplicates)];
   categories.sort();
+}
+
+function getAuthors() {
+  let withDuplicates = books.map(book => book.author);
+  authors = [...new Set(withDuplicates)];
+  authors.sort();
 }
 
 function addFilters() {
@@ -59,12 +117,51 @@ function addFilters() {
         ${categories.map(category => `<option>${category}</option>`).join('')}
       </select>
     </label>
+
+    <label><span>Filter by authors:</span>
+      <select class="authorFilter">
+        <option>all</option>
+        ${authors.map(author => `<option>${author}</option>`).join('')}
+      </select>
+    </label>
+
+    <label><span>Filter by price:</span>
+    <select class="priceFilter">
+      <option value = 0>all</option>
+      ${priceIntervals.map(priceRange => `<option>${priceRange}</option>`).join('')}
+    </select>
+  </label>
+
   `;
   document.querySelector('.categoryFilter').addEventListener(
     'change',
     event => {
       // get the selected category
       chosenCategoryFilter = event.target.value;
+      chosenAuthorFilter = 'all';
+      chosenPriceFilter = 'all';
+      // document.getElementsByClassName('.authorFilter').selectedIndex(0)
+      console.log(chosenCategoryFilter);
+      displayBooks();
+    }
+  );
+  document.querySelector('.authorFilter').addEventListener(
+    'change',
+    event => {
+      // get the selected category
+      chosenAuthorFilter = event.target.value;
+      chosenCategoryFilter = 'all';
+      chosenPriceFilter = 'all';
+      displayBooks();
+    }
+  );
+  document.querySelector('.priceFilter').addEventListener(
+    'change',
+    event => {
+      // get the selected category
+      chosenPriceFilter = event.target.value;
+      chosenAuthorFilter = 'all';
+      chosenCategoryFilter = 'all';
       displayBooks();
     }
   );
@@ -72,13 +169,39 @@ function addFilters() {
 
 function displayBooks() {
   // filter according to category and call displayBooks
-  let filteredBooks = books.filter(
+  filteredBooks = books.filter(
     ({ category }) => chosenCategoryFilter === 'all'
       || chosenCategoryFilter === category
   );
+
+  if (chosenAuthorFilter !== 'all') {
+    filteredBooks = books.filter(
+    ({ author }) => chosenAuthorFilter === author
+    );
+  }
+
+
+  if (chosenPriceFilter !== 'all') {
+    if (chosenPriceFilter === '0-40') { filteredBooks = books.filter(({ price }) => price <= 40); }
+    if (chosenPriceFilter === '40-80') { filteredBooks = books.filter(({ price }) => price <= 80 && price >= 40); } 
+    if (chosenPriceFilter === '80-120') { filteredBooks = books.filter(({ price }) => price <= 120 && price >= 80); }
+    if (chosenPriceFilter === '120-160') { filteredBooks = books.filter(({ price }) => price >= 120); }
+    // maybe an alternative way?
+    // filteredBooks = books.filter(
+    //   ({ price }) => chosenPriceFilter === 'all'
+    //     || chosenPriceFilter === price
+    // );
+  }
+
   // !! add filter for Author and Price Ranges !!
-  if (chosenSortOption === 'Title') { sortByLastName(filteredBooks); }
-  if (chosenSortOption === 'Price') { sortByPrice(filteredBooks); }
+  if (chosenSortOption === 'ID' && chosenSortOrder === 'asc') { sortById(filteredBooks); }
+  if (chosenSortOption === 'ID' && chosenSortOrder === 'des') { sortByIdDes(filteredBooks); }
+  if (chosenSortOption === 'Title' && chosenSortOrder === 'asc') { sortByTitle(filteredBooks); }
+  if (chosenSortOption === 'Title' && chosenSortOrder === 'des') { sortByTitleDes(filteredBooks); }
+  if (chosenSortOption === 'Price' && chosenSortOrder === 'asc') { sortByPrice(filteredBooks); }
+  if (chosenSortOption === 'Price' && chosenSortOrder === 'des') { sortByPriceDes(filteredBooks); }
+  if (chosenSortOption === 'Author' && chosenSortOrder === 'asc') { sortByAuthor(filteredBooks); }
+  if (chosenSortOption === 'Author' && chosenSortOrder === 'des') { sortByAuthorDes(filteredBooks); }
   // !! add sort options for Author and DESCENDING for all of them !!
   let htmlArray = filteredBooks.map(({
     id, title, author, description, category, price
